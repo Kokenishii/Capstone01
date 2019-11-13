744ec13210da53fcdc2e51723a23194a21574e4e
Unknown <ki575@nyu.edu>
2019-11-13 11:45:56 -0500

    Initial Commit

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Animator myAnimator;
+    Rigidbody rb;
+    public float speed = 1;
+    public float maxSpeed = 10;
+    public float dashForce = 5;
+    public string pNumber = "1";
+    bool canDash = true;
+    bool contact = false;
+    public float pushForce = 20f;
+   public float dashCD = 2f;
+    public GameObject banana;
+    public GameObject grenade;
+    public int bananaNum;
+    public int grenadeNum = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        Vector3 myVel = new Vector3(Input.GetAxis("Horizontal"+pNumber) * Time.deltaTime, 0f, Input.GetAxis("Vertical"+pNumber) * Time.deltaTime);
+        myVel = myVel.normalized * speed;
+
+        if (myVel.magnitude > 0.01)
+        {
+            myAnimator.SetBool("isWalking", true);
+            transform.forward = myVel;
+        }
+        else
+        {
+            myAnimator.SetBool("isWalking", false);
+        }
+        //rb.velocity = myVel;
+        if (rb.velocity.magnitude <= maxSpeed)
+        {
+            rb.AddForce(myVel, ForceMode.VelocityChange);
+        }
+          
+        if (Input.GetButtonDown("Fire"+pNumber)&&canDash&&!contact)
+        {
+            rb.AddForce(transform.forward*dashForce, ForceMode.Impulse);
+            canDash = false;
+
+            myAnimator.SetBool("isDashing", true);
+            StartCoroutine(DashCD());
+        }
+
+        if (Input.GetButtonDown("Banana" + pNumber)&& bananaNum>=1)
+        {
+            GameObject newBanana = Instantiate(banana, transform.position-1f*transform.forward+new Vector3(0,2f,0), banana.transform.rotation);
+            newBanana.GetComponent<Rigidbody>().AddForce(-transform.forward * 4f + transform.up * 2f, ForceMode.Impulse);
+            StartCoroutine(MakeBanana());
+            bananaNum -= 1;
+        }
+
+        if (Input.GetButtonDown("Grenade" + pNumber) && grenadeNum >= 1)
+        {
+            GameObject newGrenade = Instantiate(grenade, transform.position +1f * transform.forward + new Vector3(0, 2f, 0), grenade.transform.rotation);
+            newGrenade.GetComponent<Rigidbody>().AddForce(transform.forward * 5f + transform.up * 2f, ForceMode.Impulse);
+            StartCoroutine(MakeGrenade());
+            grenadeNum -= 1;
+        }
+
+
+
+    }
+
+   IEnumerator MakeBanana()
+    {
+        yield return new WaitForSeconds(5f);
+        bananaNum += 1;
+    }
+
+    IEnumerator MakeGrenade()
+    {
+        yield return new WaitForSeconds(5f);
+        grenadeNum += 1;
+    }
+
+    IEnumerator DashCD()
+    {
+        yield return new WaitForSeconds(0.5f);
+        myAnimator.SetBool("isDashing", false);
+        yield return new WaitForSeconds(dashCD);
+        canDash = true;
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.tag=="Player")
+        {
+
+            contact = true;
+            if (Input.GetButtonDown("Fire" + pNumber)&&canDash)
+            {
+                print("push");
+                collision.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * pushForce, ForceMode.Impulse);
+                canDash = false;
+                myAnimator.SetBool("isDashing", true);
+                StartCoroutine(DashCD());
+            }
+        }
+        else
+        {
+            contact = false;
+        }
+    }
+
+
+}
---
+++ Assets/PushingBox.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushingBox : MonoBehaviour
+{
+    public Vector3 moveTo = new Vector3();
+    public float moveSpeed = 5f;
+    Vector3 moveFrom = new Vector3();
+    public bool isMoving = false;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveFrom = transform.position;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+        {
+            Vector3 moveVel = (moveTo - moveFrom) * Time.deltaTime*moveSpeed;
+            rb.velocity = moveVel;
+            if ((transform.position - moveTo).magnitude <= 0.5f)
+            {
+                isMoving = false;
+                Vector3 temp = moveTo;
+                moveTo = moveFrom;
+                moveFrom = temp;
+            }
+        }
+
+      
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+//        print(other.gameObject.tag);
+
+
+        if ((Input.GetButtonDown("Fire1") && other.gameObject.name =="Player1")
+        ||( Input.GetButtonDown("Fire2") && other.gameObject.name == "Player2"))
+        {
+          // print(other.gameObject.tag);
+            isMoving = true;
+        }
+    }
+}
